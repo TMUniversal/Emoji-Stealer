73eebc641a398d8a283e7270043b6eb40ee9514d
Universal Studio <uni@un1.pw>
2020-06-30 18:45:19 +0200

    Fix exit routine

--- src/client/BotClient.ts
+++ src/client/BotClient.ts
@@ -99,11 +99,7 @@ export default class BotClient extends AkairoClient {
 
     //  Process handling
     process.once('SIGINT', () => {
-      this.logger.info(`[${this.user.username}] Process exit.`)
-      this.destroy()
-    })
-    process.on('exit', () => {
-      this.logger.info(`[${this.user.username}] Process exit.`)
+      this.logger.warn(`[${this.user.username}] Received SIGINT => Quitting.`)
       this.destroy()
       process.exit(0)
     })
