297df2b8135e2537bfef79b6a8c3afdbd0e02b91
Universal Studio <uni@un1.pw>
2020-06-30 17:58:08 +0200

    Create AboutCommand.ts

--- src/client/BotClient.ts
+++ src/client/BotClient.ts
@@ -105,6 +105,7 @@ export default class BotClient extends AkairoClient {
     process.on('exit', () => {
       this.logger.info(`[${this.user.username}] Process exit.`)
       this.destroy()
+      process.exit(0)
     })
     process.on('uncaughtException', (err: Error) => {
       const errorMsg = (err ? err.stack || err : '').toString().replace(new RegExp(`${__dirname}/`, 'g'), './')
