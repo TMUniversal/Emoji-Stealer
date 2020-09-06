5bc7c13fb9f4b81f24bc90d2b6e27f9b876a748b
Universal Studio <uni@un1.pw>
2020-09-06 09:37:53 +0200

    chore: Add version to embed footer

--- src/commands/basic/InviteCommand.ts
+++ src/commands/basic/InviteCommand.ts
@@ -1,6 +1,7 @@
 import { Command } from 'discord-akairo'
 import { Message } from 'discord.js'
 import { MessageEmbed } from '../../structures/MessageEmbed'
+import config from '../../config'
 
 export default class InviteCommand extends Command {
   public constructor () {
@@ -19,6 +20,6 @@ export default class InviteCommand extends Command {
   }
 
   public exec (message: Message): Promise<Message> {
-    return message.util.send(new MessageEmbed().setDescription(`[**Click here to add me to your server**](https://discord.com/api/oauth2/authorize?client_id=${this.client.user.id}&permissions=1074072576&scope=bot)`).setFooter(this.client.user.username, this.client.user.avatarURL({ dynamic: true })))
+    return message.util.send(new MessageEmbed().setDescription(`[**Click here to add me to your server**](https://discord.com/api/oauth2/authorize?client_id=${this.client.user.id}&permissions=1074072576&scope=bot)`).setFooter(`${this.client.user.username} v${config.version}`, this.client.user.avatarURL({ dynamic: true })))
   }
 }
